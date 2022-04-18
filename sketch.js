2800ec6805348fa321da630e88b66ce371b5e4f4
Gabriel-aqui <91337220+Gabriel-aqui@users.noreply.github.com>
2022-04-18 15:57:27 -0300

    v1
    github é complicado

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var kadrado;
+
+function setup() {
+  createCanvas(1000,500);
+  kadrado = createSprite(500, 250, 25, 25);
+  kadrado.shapeColor = "cyan";
+}
+
+function draw() {
+  background(30);
+  drawSprites()
+}
+
+
+
+
